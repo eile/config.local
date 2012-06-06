3df22dbf8c3cece90d920f859559298f930f5883
Stefan Eilemann <eilemann@gmail.com>
2012-06-06 16:59:26 +0200

    forks update

--- forks.cmake
+++ forks.cmake
@@ -1,2 +1,3 @@
 set(CODASH_USER_URL https://github.com/eile/codash.git)
 set(QT_STEERER_USER_URL https://github.com/eile/qt_steerer.git)
+set(STEER_LIB_USER_URL https://github.com/eile/steer_lib.git)

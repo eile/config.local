8f131c3804d2e20aaf94c5826982428a98144820
Stefan Eilemann <eilemann@gmail.com>
2012-06-06 14:31:00 +0200

    Fix fork

--- forks.cmake
+++ forks.cmake
@@ -1,2 +1,2 @@
 set(CODASH_USER_URL https://github.com/eile/codash.git)
-set(QT_STEERER_USER_URL https://github.com/BlueBrain/qt_steerer.git)
+set(QT_STEERER_USER_URL https://github.com/eile/qt_steerer.git)

970fa5c99684dd116c99dfa20bc4209ec1d9fc81
Stefan Eilemann <eilemann@gmail.com>
2012-10-30 16:23:46 +0100

    Update my user forks

--- forks.cmake
+++ forks.cmake
@@ -1,4 +1,5 @@
 set(CODASH_USER_URL https://github.com/eile/codash.git)
+set(VMMLIB_USER_URL https://github.com/eile/vmmlib.git)
 set(QT_STEERER_USER_URL https://github.com/eile/qt_steerer.git)
 set(STEER_LIB_USER_URL https://github.com/eile/steer_lib.git)
-set(CTOOLS_USER_URL https://github.com/BlueBrain/ctools.git)
+set(MGL_USER_URL https://github.com/BlueBrain/MGL.git)
